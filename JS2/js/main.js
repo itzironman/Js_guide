4cb5a013ccd0c6c1f7cac99c5ae11e28c90ae6da
Md. Zahid Hasan <35896950+itzironman@users.noreply.github.com>
2022-10-19 18:36:16 +0600

    Day 1
    Start my js journey.

---
+++ JS2/js/main.js
@@ -0,0 +1,46 @@
+
+console.log("Hello world");
+console.log("This is Zahid.");
+
+//checking the data type -- 
+console.log(typeof "Dave");
+console.log(typeof 44);
+console.log(typeof true);
+
+console.log("---------------------------------");
+
+//Veriable declare
+const myVariable = "Mathematics";
+
+//veriable length count
+console.log(myVariable.length);
+console.log("This is a string".length);
+
+//find letter in the veriable
+console.log(myVariable.charAt(5));
+console.log("This ia a para".charAt(6));
+
+//find in the variable
+console.log(myVariable.indexOf("cs"));
+console.log(myVariable.lastIndexOf("mat"));
+
+//slice the word
+console.log(myVariable.slice(1, 5));
+
+//change the case
+console.log(myVariable.toUpperCase());
+console.log(myVariable.toLowerCase());
+
+//check the letter of word is in the variable or not
+console.log(myVariable.includes("div"));
+console.log(myVariable.includes("mat"));
+
+//split the variable
+console.log(myVariable.split("e"));
+console.log(myVariable.split("a"));
+console.log(myVariable.split(""));
+
+//split the sentence
+console.log("John, Joe, Dave".split(","));
+console.log("Every good boy does fine".split(" "));
+
